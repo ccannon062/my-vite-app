0a870550844155dfa6533dfa705398cdb8c20ca4
ccannon062 <caleb.cristoph.cannon@gamil.com>
2025-10-18 16:48:59 -0500

    Content added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Credit to Shadcn for the components
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="relative bg-[url('/bg-image.jpg')] w-lvw h-lvh bg-no-repeat bg-cover bg-center">
+        <div className="absolute inset-0 bg-black/40 backdrop-blur-sm"></div>
+        <div className="relative z-10 flex items-center justify-center h-full px-8 py-8">
+          <div className="flex items-stretch gap-4">
+            <Card className="w-full max-w-sm bg-white/10 backdrop-blur-md border border-white/10 text-white shadow-lg">
+              <CardContent>
+                <img
+                  src="./public/headshot.jpg"
+                  alt="Headshot photo"
+                  className="rounded-sm"
+                />
+              </CardContent>
+            </Card>
+            <Card className="w-full max-w-2xl bg-white/10 backdrop-blur-md border border-white/10 text-white shadow-lg">
+              <CardHeader>
+                <CardTitle>About Me</CardTitle>
+                <CardDescription className="text-gray-300">
+                  Student & Software Developer
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  I'm a senior Computer Science major at Creighton University,
+                  minoring in Philosophy. I'm passionate about desigining and
+                  building beautiful, simple software. My experience as an
+                  intern at First National Bank of Omaha and as a web developer
+                  for my schools "iJay practicum," are just a few of the many
+                  different fields where I've applied my skills. To see more of
+                  my work please visit my portfolio page linked below.
+                </p>
+              </CardContent>
+              <CardFooter>
+                <a href="https://ccannon.dev" className="text-gray-300">
+                  ccannon.dev
+                </a>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
